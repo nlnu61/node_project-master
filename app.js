019307beccaabcf87b46ba2986e0cf12738033aa
nlnu61 <navdeepkaur.lnu9@rdpolytech.ca>
2025-03-19 09:30:33 -0600

    Update

--- app.js
+++ app.js
@@ -11,9 +11,16 @@ app.get('/home', (req, res) =>{
     res.send("Hello, World!");
 });
 
-//route handler for a post request
-app.post('/response', (req, res) =>{
-    res.send('This is a post response in RESTful style');
+//route handler for a post request using path parameter
+app.post('//user/:id', (req, res) =>{
+    const user_id = req.params.id;
+    res.send(`This is the user id: ${user_id}`);
+});
+
+//Route with a query parameter
+app.get('/search', (req, res) =>{
+    const query = req.query.q;
+    res.send(`Search query is ${query}`);
 });
 
 //Start our server, listening on port 3000
