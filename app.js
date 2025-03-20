5d8da3a4d7c65cc22c45343dfa04bfe1ebb3856c
nlnu61 <navdeepkaur.lnu9@rdpolytech.ca>
2025-03-20 12:06:22 -0600

    Update

--- app.js
+++ app.js
@@ -1,29 +1,13 @@
-//import the express package
 const express = require('express');
-require('dotenv').config();
-const PORT = process.env.PORT;
+const router = require(" ./routes/pages");
 
-//initialize a new express instance equal to the app object
 const app = express();
+const PORT = 3000;
 
-//route handler that sends back a response
-app.get('/home', (req, res) =>{
-    res.send("Hello, World!");
-});
+//implementing routes
+app.use(router);
 
-//route handler for a post request using path parameter
-app.post('//user/:id', (req, res) =>{
-    const user_id = req.params.id;
-    res.send(`This is the user id: ${user_id}`);
-});
 
-//Route with a query parameter
-app.get('/search', (req, res) =>{
-    const query = req.query.q;
-    res.send(`Search query is ${query}`);
-});
-
-//Start our server, listening on port 3000
-app.listen(PORT, () =>{
-    console.log(`Connected on port ${PORT}`);
+app.listen(PORT, ()=>{
+    console.log(`Server is running on http://localhost: ${PORT}`);
 });
